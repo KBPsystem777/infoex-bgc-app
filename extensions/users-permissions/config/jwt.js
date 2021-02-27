21643b14660cd12d86612d3cd1b8e5e0278e99d0
kbpsystem777 <koleen.bp@outlook.com>
2021-02-28 01:28:05 +0800

    halfway deployment

--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,6 +1,6 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || "f2aa8b29-9a6b-4de8-a14b-5203531bca50",
+  jwtSecret: process.env.JWT_SECRET,
   jwt: {
-    expiresIn: "12h",
+    expiresIn: "7d",
   },
 };
